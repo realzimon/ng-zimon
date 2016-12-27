55db54a4f3ca9ebb84df0e73dcc7ad6baa7de726
Literallie <devnull@nowak-at.net>
2016-12-27 14:48:45 +0100

    Fix tslint errors

--- app/navbar.component.ts
+++ app/navbar.component.ts
@@ -1,19 +1,19 @@
 /**
- * Created by DZDomi on 27.12.16.
+ * Component for the main navigation bar.
  */
 
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-    selector: 'navbar',
-    templateUrl: '../templates/navbar.component.html'
+  selector: 'navbar',
+  templateUrl: '../templates/navbar.component.html'
 
 })
-export class NavComponent  {
-    randomQuote = '#danielvcs';
-    time = new Date();
+export class NavComponent {
+  randomQuote = '#danielvcs';
+  time = new Date();
 
-    constructor(){
-        window.setInterval(() => this.time = new Date(), 1000);
-    }
+  constructor() {
+    window.setInterval(() => this.time = new Date(), 1000);
+  }
 }
