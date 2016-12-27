f4812b7edc6c00ec6034a129500016eaaf3e77fc
Literallie <devnull@nowak-at.net>
2016-12-27 16:19:11 +0100

    Fix tslint errors in navbar component

--- app/navbar/navbar.component.ts
+++ app/navbar/navbar.component.ts
@@ -2,39 +2,39 @@
  * Component for the main navigation bar.
  */
 
-import { Component } from '@angular/core';
-import { QuotesService } from "../services/quotes.service";
+import {Component} from '@angular/core';
+import {QuotesService} from '../services/quotes.service';
 
 @Component({
-    selector: 'navbar',
-    templateUrl: 'app/navbar/navbar.component.html',
-    host: {
-        '(document:keydown)': 'handleKeyboardEvent($event)'
-    }
+  selector: 'navbar',
+  templateUrl: 'app/navbar/navbar.component.html',
+  host: {
+    '(document:keydown)': 'handleKeyboardEvent($event)'
+  }
 })
-export class NavComponent  {
-    randomQuote = 'Random Quote';
-    time = new Date();
+export class NavComponent {
+  randomQuote = 'Random Quote';
+  time = new Date();
 
-    constructor(private quoteService: QuotesService){
-        window.setInterval(() => this.time = new Date(), 1000);
-        window.setInterval(() => {
-            quoteService.getRandomQuote()
-                .subscribe(quote => this.randomQuote = quote);
-        }, 10000);
-        quoteService.getRandomQuote()
-            .subscribe(quote => this.randomQuote = quote);
-    }
+  constructor(private quoteService: QuotesService) {
+    window.setInterval(() => this.time = new Date(), 1000);
+    window.setInterval(() => {
+      quoteService.getRandomQuote()
+        .subscribe(quote => this.randomQuote = quote);
+    }, 10000);
+    quoteService.getRandomQuote()
+      .subscribe(quote => this.randomQuote = quote);
+  }
 
-    loadNewQuote(){
-        this.quoteService.getRandomQuote()
-            .subscribe(quote => this.randomQuote = quote);
-    }
+  loadNewQuote() {
+    this.quoteService.getRandomQuote()
+      .subscribe(quote => this.randomQuote = quote);
+  }
 
-    handleKeyboardEvent(event: KeyboardEvent){
-        //The lowercase letter 'q'
-        if(event.keyCode === 81){
-            this.loadNewQuote();
-        }
+  handleKeyboardEvent(event: KeyboardEvent) {
+    // The lowercase letter 'q'
+    if (event.keyCode === 81) {
+      this.loadNewQuote();
     }
+  }
 }
