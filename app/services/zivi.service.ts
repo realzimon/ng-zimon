9cb0c0a02aeb55ef8dedbb117e26cb91118be77b
Literallie <devnull@nowak-at.net>
2016-12-29 08:43:31 +0100

    Add colorHex to Zivi model, Fix indentation of zivi.service

--- app/services/zivi.service.ts
+++ app/services/zivi.service.ts
@@ -12,6 +12,7 @@ export class Zivi {
               public name_mx: string,
               public post_count: number,
               public color: string,
+              public colorHex: string,
               public picture: string,
               public first: number) {
 
@@ -22,33 +23,34 @@ export class Zivi {
 @Injectable()
 export class ZiviService {
 
-    private url = 'http://localhost:4000/api/v1/zivis';
+  private url = 'http://localhost:4000/api/v1/zivis';
 
-    static createPictureUrl(url: string) {
-        return 'http://localhost:4000/images/' + url;
-    }
+  static createPictureUrl(url: string) {
+    return 'http://localhost:4000/images/' + url;
+  }
 
-    constructor(private http: Http) {
-    }
+  constructor(private http: Http) {
+  }
 
-    getZiviByName(name: string) {
-      return new Zivi(name, 'name', 0, 'teal', 'picture', 0);
-    }
+  getZiviByName(name: string) {
+    return new Zivi(name, 'name', 0, 'teal', '#009688', 'picture', 0);
+  }
 
-    getAllZivis() {
-        return this.http.get(this.url)
-            .map(res => res.json())
-            .map(res => {
-                let zivis: Zivi[] = [];
-                res.zivis.forEach(function(zivi: any){
-                    zivis.push(new Zivi(zivi.name,
-                      zivi.name_mx,
-                      zivi.post_count,
-                      zivi.color,
-                      ZiviService.createPictureUrl(zivi.picture),
-                      zivi.first));
-                });
-                return zivis;
-            });
-    }
+  getAllZivis() {
+    return this.http.get(this.url)
+      .map(res => res.json())
+      .map(res => {
+        let zivis: Zivi[] = [];
+        res.zivis.forEach(function (zivi: any) {
+          zivis.push(new Zivi(zivi.name,
+            zivi.name_mx,
+            zivi.post_count,
+            zivi.color,
+            zivi.colorHex,
+            ZiviService.createPictureUrl(zivi.picture),
+            zivi.first));
+        });
+        return zivis;
+      });
+  }
 }
