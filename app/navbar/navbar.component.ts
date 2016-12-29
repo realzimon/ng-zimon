76cf396899f2e51941b22104cf357601ce9a8ec2
DZDomi <dzdomi@gmail.com>
2016-12-29 09:06:28 +0100

    Bugfixes for typescript syntax

--- app/navbar/navbar.component.ts
+++ app/navbar/navbar.component.ts
@@ -4,7 +4,7 @@
 
 import {Component} from '@angular/core';
 import {QuotesService} from '../services/quotes.service';
-import {TimerService} from "../services/timer.service";
+import {TimerService} from '../services/timer.service';
 
 @Component({
   selector: 'navbar',
@@ -20,7 +20,7 @@ export class NavComponent {
   constructor(private quoteService: QuotesService, private timerService: TimerService) {
     timerService.getTimerUpdates().subscribe((data: any) => {
       this.time = new Date();
-      if(data.remaining % 10 === 0){
+      if (data.remaining % 10 === 0) {
         this.loadNewQuote();
       }
     });
