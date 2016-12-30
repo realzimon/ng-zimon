49b48c5825c3fa0b7f4df4eb9fb6be60d3104cdd
DZDomi <dzdomi@gmail.com>
2016-12-30 16:32:30 +0100

    Added production mode variable

--- app/main.ts
+++ app/main.ts
@@ -2,4 +2,11 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app.module';
 
+import { ENV } from './config/environment';
+import {enableProdMode} from "@angular/core";
+
+if(ENV.production){
+    enableProdMode();
+}
+
 platformBrowserDynamic().bootstrapModule(AppModule);
