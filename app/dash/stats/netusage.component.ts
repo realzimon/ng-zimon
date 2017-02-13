456604d2fa5783e007b05b8552cfc6a3d883171e
DZDomi <dzdomi@gmail.com>
2017-02-13 10:16:22 +0100

    removed animation

--- app/dash/stats/netusage.component.ts
+++ app/dash/stats/netusage.component.ts
@@ -26,7 +26,8 @@ export class NetUsage {
                     zeroLineColor: '#ffffff'
                 }
             }]
-        }
+        },
+        animation: false
     };
 
     public barChartLabels: string[] = [];
