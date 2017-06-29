97f1591100aeac61457893a11053ace083c40869
Literallie <git@l1t.li>
2017-06-29 14:21:09 +0200

    Copy undefined properties from sample env to local env

--- app/config/sample-environment.ts
+++ app/config/sample-environment.ts
@@ -1,3 +1,9 @@
+/*
+Copy this file to environment.ts and change accordingly.
+Note that you do not need to specify all properties since
+any missing ones will be automatically inherited from the defaults.
+ */
+
 export const ENV = {
   production: false,
   backendUrl: 'http://localhost:4000/',
--- app/main.ts
+++ app/main.ts
@@ -1,12 +1,24 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app.module';
+import {AppModule} from './app.module';
 
-import { ENV } from './config/environment';
+import {ENV} from './config/environment';
+import {ENV as SAMPLE_ENV} from './config/sample-environment';
 import {enableProdMode} from "@angular/core";
 
-if(ENV.production){
-    enableProdMode();
+if (ENV.production) {
+  enableProdMode();
+}
+
+for (let prop in SAMPLE_ENV) {
+  if (ENV[prop] === undefined) {
+    console.info('Using default value for config property', prop, 'because it is not defined');
+    ENV[prop] = SAMPLE_ENV[prop];
+  }
+}
+
+if (!ENV.socketUrl) {
+  ENV.socketUrl = SAMPLE_ENV.socketUrl;
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule).catch(err => {
