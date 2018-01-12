0593858c32c37ca2b69450358d5eb2223b8e0f0a
Benjamin Ableidinger <benjamin.ableidinger@gmail.com>
2018-01-12 15:38:01 +0100

    design overhaul, hides poststats/postler

---
+++ app/dash/tmstats/tmstats.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
+
+
+@Component({
+  selector: 'tmstats',
+  templateUrl: 'app/dash/tmstats/tmstats.component.html'
+})
+export class TmstatsComponent implements OnInit{
+
+}
