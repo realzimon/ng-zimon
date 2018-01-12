34c94d892791e6c8f2f3493c33f32322ac1dc829
Benjamin Ableidinger <benjamin.ableidinger@gmail.com>
2018-01-12 12:49:13 +0100

    fixes

--- app/services/tm.service.ts
+++ app/services/tm.service.ts
@@ -5,7 +5,6 @@ import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
 import { Http } from '@angular/http';
 import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
-import { setInterval } from 'timers';
 
 @Injectable()
 export class TmService implements OnInit {
@@ -25,14 +24,12 @@ export class TmService implements OnInit {
     .switchMap(() => http.get('http://localhost:8000/GetCurrentRanking')).map((data) => data.json())
     .subscribe((data) => {
       this.times = data;
-      console.log(data);
     });
 
     Observable.interval(2000)
     .switchMap(() => http.get('http://localhost:8000/GetCurrentChallengeInfo')).map((data) => data.json())
     .subscribe((data) => {
-      this.track = data["Name"];
-      console.log(this.track);
+      this.track = data['Name'];
     });
   }
 
